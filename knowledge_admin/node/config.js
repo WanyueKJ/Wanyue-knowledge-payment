b2a0578a38b230ae05ee5fd7e22a605ca8b1c218
shuyinghengxieshuiqingqian <lj5211zl@163.com>
2021-03-31 17:11:24 +0800

    # task 目录更新

---
+++ knowledge_admin/node/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	'REDISHOST' : '127.0.0.1',
+	'REDISPASS' : '123456',
+	'REDISPORT' : '6379',
+	'TOKEN'		: '1234567',
+	'sign_key'	: '400d069a791d51ada8af3e6c2979bcd7',
+	'socket_port': '19967',
+	'WEBADDRESS': 'https://demo.sdwanyue.com/api/',  //站点域名
+	'WEBSITE': 'https://demo.sdwanyue.com'  //站点域名
+}
